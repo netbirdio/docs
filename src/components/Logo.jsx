d359c11b761b2ef25661f9adbc83a4e7e322c9a4
Pascal Fischer <pascal@netbird.io>
2023-05-09 16:15:22 +0200

    move logo into its own folder

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 export function Logo(props) {
   return (
       <div>
-          <img src='/img/logo.svg' alt="some file"  height='200'
+          <img src='/img/logo/logo.svg' alt="some file"  height='200'
                width='180' className="dark:hidden"/>
-          <img src='/img/logo-dark.svg' alt="some file"  height='200'
+          <img src='/img/logo/logo-dark.svg' alt="some file"  height='200'
                width='180' className="hidden dark:block"/>
       </div>
   )
