5e27b0f37e7b912e26386fbc8dda5e926996b7c0
Pascal Fischer <pascal@netbird.io>
2023-05-09 16:32:25 +0200

    separate logo for API

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,17 @@
+import {useRouter} from "next/router";
+
 export function Logo(props) {
+    let router = useRouter()
   return (
       <div>
-          <img src='/img/logo/logo.svg' alt="some file"  height='200'
-               width='180' className="dark:hidden"/>
-          <img src='/img/logo/logo-dark.svg' alt="some file"  height='200'
-               width='180' className="hidden dark:block"/>
+          {router.route.startsWith('/docs') && <img src='/img/logo/logo-docs.svg' alt="some file"  height='200'
+               width='180' className="dark:hidden"/> ||
+          <img src='/img/logo/logo-docs-dark.svg' alt="some file"  height='200'
+               width='180' className="hidden dark:block"/>}
+          {router.route.startsWith('/ipa') && <img src='/img/logo/logo-api.svg' alt="some file"  height='200'
+               width='180' className="dark:hidden"/> ||
+          <img src='/img/logo/logo-api-dark.svg' alt="some file"  height='200'
+               width='180' className="hidden dark:block"/>}
       </div>
   )
 }
