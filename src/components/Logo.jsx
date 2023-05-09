c9200f4bea866b035b9ed7bc12f581cd0779a97a
Pascal Fischer <pascal@netbird.io>
2023-05-09 17:23:43 +0200

    fix logos

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,12 +5,12 @@ export function Logo(props) {
   return (
       <div>
           {router.route.startsWith('/docs') && <img src='/img/logo/logo-docs.svg' alt="some file"  height='200'
-               width='180' className="dark:hidden"/> ||
-          <img src='/img/logo/logo-docs-dark.svg' alt="some file"  height='200'
+               width='180' className="dark:hidden"/>}
+          {router.route.startsWith('/docs') &&  <img src='/img/logo/logo-docs-dark.svg' alt="some file"  height='200'
                width='180' className="hidden dark:block"/>}
           {router.route.startsWith('/ipa') && <img src='/img/logo/logo-api.svg' alt="some file"  height='200'
-               width='180' className="dark:hidden"/> ||
-          <img src='/img/logo/logo-api-dark.svg' alt="some file"  height='200'
+               width='180' className="dark:hidden"/>}
+          {router.route.startsWith('/ipa') && <img src='/img/logo/logo-api-dark.svg' alt="some file"  height='200'
                width='180' className="hidden dark:block"/>}
       </div>
   )
