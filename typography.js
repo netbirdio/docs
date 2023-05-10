45268d90fa26d9abbf3bc205bee167ea794cf738
Pascal Fischer <pascal@netbird.io>
2023-05-10 20:56:41 +0200

    back to original heading style

--- typography.js
+++ typography.js
@@ -180,19 +180,12 @@ module.exports = ({ theme }) => ({
       // Headings
       h1: {
         color: 'var(--tw-prose-headings)',
-        fontWeight: '800',
-        fontSize: theme('fontSize.3xl')[0],
-        ...theme('fontSize.2xl')[1],
-        marginBottom: theme('spacing.2'),
-      },
-      h2: {
-        color: 'var(--tw-prose-headings)',
         fontWeight: '700',
         fontSize: theme('fontSize.2xl')[0],
         ...theme('fontSize.2xl')[1],
         marginBottom: theme('spacing.2'),
       },
-      h3: {
+      h2: {
         color: 'var(--tw-prose-headings)',
         fontWeight: '600',
         fontSize: theme('fontSize.lg')[0],
@@ -200,27 +193,11 @@ module.exports = ({ theme }) => ({
         marginTop: theme('spacing.16'),
         marginBottom: theme('spacing.2'),
       },
-      h4: {
+      h3: {
         color: 'var(--tw-prose-headings)',
         fontSize: theme('fontSize.base')[0],
         ...theme('fontSize.base')[1],
-        fontWeight: '500',
-        marginTop: theme('spacing.10'),
-        marginBottom: theme('spacing.2'),
-      },
-      h5: {
-        color: 'var(--tw-prose-headings)',
-        fontSize: theme('fontSize.base')[0],
-        ...theme('fontSize.base')[1],
-        fontWeight: '400',
-        marginTop: theme('spacing.10'),
-        marginBottom: theme('spacing.2'),
-      },
-      h6: {
-        color: 'var(--tw-prose-headings)',
-        fontSize: theme('fontSize.base')[0],
-        ...theme('fontSize.base')[1],
-        fontWeight: '300',
+        fontWeight: '600',
         marginTop: theme('spacing.10'),
         marginBottom: theme('spacing.2'),
       },
