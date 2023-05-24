99398088f845b363bc495768a1b349c77dc15213
braginini <bangvalo@gmail.com>
2023-05-24 16:23:39 +0200

    Fix old /docs redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,57 @@
 import nextMDX from '@next/mdx'
-import { remarkPlugins } from './mdx/remark.mjs'
-import { rehypePlugins } from './mdx/rehype.mjs'
-import { recmaPlugins } from './mdx/recma.mjs'
+import {remarkPlugins} from './mdx/remark.mjs'
+import {rehypePlugins} from './mdx/rehype.mjs'
+import {recmaPlugins} from './mdx/recma.mjs'
 
 const withMDX = nextMDX({
-  options: {
-    remarkPlugins,
-    // rehypeSlug,
-    rehypePlugins,
-    recmaPlugins,
-    providerImportSource: '@mdx-js/react',
-  },
+    options: {
+        remarkPlugins,
+        // rehypeSlug,
+        rehypePlugins,
+        recmaPlugins,
+        providerImportSource: '@mdx-js/react',
+    },
 })
 
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/docs-static',
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    scrollRestoration: true,
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/introduction',
-      },
-      {
-        source: '/api',
-        destination: '/ipa/introduction',
-      },
-      {
-        source: '/api/:path*',
-        destination: '/ipa/:path*',
-      }
-    ]
-  }
+    assetPrefix: '/docs-static',
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+    experimental: {
+        scrollRestoration: true,
+    },
+    redirects: async () => {
+        return [
+            {
+                source: '/docs',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/docs/:path*',
+                destination: '/:path*',
+                permanent: true,
+            }
+        ]
+    },
+    rewrites: async () => {
+        return [
+            {
+                source: '/',
+                destination: '/introduction',
+            },
+            {
+                source: '/api',
+                destination: '/ipa/introduction',
+            },
+            {
+                source: '/api/:path*',
+                destination: '/ipa/:path*',
+            }
+        ]
+    }
 }
 
 export default withMDX(nextConfig)
