27af84d391149067f374c2eb54c99838e7891fc9
braginini <bangvalo@gmail.com>
2023-05-25 14:56:37 +0200

    Redirect old docs

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,61 @@ const nextConfig = {
     redirects: async () => {
         return [
             {
+                source: '/docs/getting-started/installation',
+                destination: '/how-to/getting-started#installation',
+                permanent: true,
+            },
+            {
+                source: '/docs/overview/personal-access-tokens',
+                destination: '/how-to/access-netbird-public-api',
+                permanent: true,
+            },
+            {
+                source: '/docs/overview/personal-access-tokens',
+                destination: '/how-to/access-netbird-public-api',
+                permanent: true,
+            },
+            {
+                source: '/docs/overview/acls',
+                destination: '/how-to/manage-network-access',
+                permanent: true,
+            },
+            {
+                source: '/docs/how-to-guides/nameservers',
+                destination: '/how-to/manage-dns-in-your-network',
+                permanent: true,
+            },
+            {
+                source: '/docs/how-to-guides/nameservers',
+                destination: '/how-to/manage-dns-in-your-network',
+                permanent: true,
+            },
+            {
+                source: '/docs/how-to-guides/network-routes',
+                destination: '/routing-traffic-to-private-networks',
+                permanent: true,
+            },
+            {
+                source: '/docs/overview/setup-keys',
+                destination: '/how-to/register-machines-using-setup-keys',
+                permanent: true,
+            },
+            {
+                source: '/docs/how-to-guides/activity-monitoring',
+                destination: '/how-to/monitor-system-and-network-activity',
+                permanent: true,
+            },
+            {
+                source: '/docs/how-to-guides/periodic-authentication',
+                destination: '/how-to/enforce-periodic-user-authentication',
+                permanent: true,
+            },
+            {
+                source: '/docs/overview/setup-keys',
+                destination: '/how-to/register-machines-using-setup-keys',
+                permanent: true,
+            },
+            {
                 source: '/docs',
                 destination: '/',
                 permanent: true,
