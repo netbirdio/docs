0f1cdf5273f28118a316f50effab60e968ce26ef
braginini <bangvalo@gmail.com>
2024-04-22 14:32:31 +0200

    Fix activity event streaming broken links

--- src/components/NavigationDocs.jsx
+++ src/components/NavigationDocs.jsx
@@ -102,7 +102,7 @@ export const docsNavigation = [
              title: 'Integrations',
              isOpen: false,
              links: [
-                 {title: 'Activity event streaming', href: '/how-to/activity-event-streaming-to-siem-systems' },
+                 {title: 'Activity event streaming', href: '/how-to/activity-event-streaming' },
                  {title: 'Identity provider sync', href: '/how-to/idp-sync' },
                  {title: 'Enable post quantum cryptography', href: '/how-to/enable-post-quantum-cryptography' },
                  {title: 'Endpoint detection and response (EDR)', href: '/how-to/endpoint-detection-and-response' },
