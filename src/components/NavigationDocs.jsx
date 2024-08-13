ac8049ac2202abe3fa1955aab5d97f20365bd1ed
braginini <bangvalo@gmail.com>
2024-08-13 17:58:14 +0200

    Remove references to activity streaming and idp-sync from integrations

--- src/components/NavigationDocs.jsx
+++ src/components/NavigationDocs.jsx
@@ -121,8 +121,6 @@ export const docsNavigation = [
              title: 'Integrations',
              isOpen: false,
              links: [
-                 {title: 'Activity event streaming', href: '/how-to/activity-event-streaming' },
-                 {title: 'Identity provider sync', href: '/how-to/idp-sync' },
                  {title: 'Enable post quantum cryptography', href: '/how-to/enable-post-quantum-cryptography' },
                  {title: 'Endpoint detection and response (EDR)', href: '/how-to/endpoint-detection-and-response' },
              ]
