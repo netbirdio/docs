9411988d8a782535f496c7608d2073878f0e8333
Maycon Santos <mlsmaycon@gmail.com>
2024-11-22 18:09:04 +0100

    Update slack invite link

--- src/components/NavigationAPI.jsx
+++ src/components/NavigationAPI.jsx
@@ -46,7 +46,7 @@ export function NavigationAPI({tableOfContents, className}) {
           <TopLevelNavItem href="/api">API</TopLevelNavItem>
           <TopLevelNavItem href="https://netbird.io/knowledge-hub/">Learn</TopLevelNavItem>
           <TopLevelNavItem href="https://github.com/netbirdio/netbird">Github</TopLevelNavItem>
-          <TopLevelNavItem href="https://join.slack.com/t/netbirdio/shared_invite/zt-2p5zwhm4g-8fHollzrQa5y4PZF5AEpvQ">Support</TopLevelNavItem>
+          <TopLevelNavItem href="https://join.slack.com/t/netbirdio/shared_invite/zt-2utg2ncdz-W7LEB6toRBLE1Jca37dYpg">Support</TopLevelNavItem>
           {apiNavigation.map((group, groupIndex) => (
               <NavigationStateProvider key={group.title} index={groupIndex}>
                 <NavigationGroup
