2854e68181f34aa77168107bcee38bc88b6a24f1
Viktor Liu <viktor@netbird.io>
2026-04-14 12:55:15 +0200

    Add CrowdSec IP reputation documentation

--- src/components/NavigationDocs.jsx
+++ src/components/NavigationDocs.jsx
@@ -550,6 +550,10 @@ export const docsNavigation = [
             title: 'Management Geolocation Database',
             href: '/selfhosted/geo-support',
           },
+          {
+            title: 'CrowdSec IP Reputation',
+            href: '/selfhosted/maintenance/crowdsec',
+          },
         ],
       },
       {
