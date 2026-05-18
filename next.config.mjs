2b79407215680b2607dab52b8ca3da27d98965ab
Brandon Hopkins <brandon@techhut.tv>
2026-05-18 11:20:36 -0700

    Clean up redirects

--- next.config.mjs
+++ next.config.mjs
@@ -82,11 +82,6 @@ const nextConfig = {
                 permanent: true,
             },
             {
-                source: '/docs/overview/setup-keys',
-                destination: '/manage/peers/register-machines-using-setup-keys',
-                permanent: true,
-            },
-            {
                 source: '/docs',
                 destination: '/',
                 permanent: true,
@@ -262,7 +257,7 @@ const nextConfig = {
             },
             {
                 source: '/how-to/db-workload-migration',
-                destination: '/manage/peers/site-to-site/db-workload-migration',
+                destination: '/manage/network-routes/use-cases/site-to-site',
                 permanent: true,
             },
             // documentation redirects for use-cases
@@ -299,22 +294,22 @@ const nextConfig = {
             },
             {
                 source: '/how-to/routing-traffic-to-multiple-resources',
-                destination: '/manage/networks/routing-traffic-to-multiple-resources',
+                destination: '/manage/networks',
                 permanent: true,
             },
             {
                 source: '/how-to/accessing-restricted-domain-resources',
-                destination: '/manage/networks/accessing-restricted-domain-resources',
+                destination: '/manage/networks',
                 permanent: true,
             },
             {
                 source: '/how-to/accessing-entire-domains-within-networks',
-                destination: '/manage/networks/accessing-entire-domains-within-networks',
+                destination: '/manage/networks',
                 permanent: true,
             },
             {
                 source: '/how-to/access-home-network',
-                destination: '/manage/networks/homelab/access-home-network',
+                destination: '/manage/networks/use-cases/access-home-devices',
                 permanent: true,
             },
             // documentation redirects for network-routes
@@ -325,17 +320,17 @@ const nextConfig = {
             },
             {
                 source: '/how-to/configuring-default-routes-for-internet-traffic',
-                destination: '/manage/network-routes/configuring-default-routes-for-internet-traffic',
+                destination: '/manage/network-routes/use-cases/exit-nodes',
                 permanent: true,
             },
             {
                 source: '/how-to/configuring-routes-with-access-control',
-                destination: '/manage/network-routes/configuring-routes-with-access-control',
+                destination: '/manage/network-routes/access-control',
                 permanent: true,
             },
             {
                 source: '/how-to/resolve-overlapping-routes',
-                destination: '/manage/network-routes/resolve-overlapping-routes',
+                destination: '/manage/network-routes/overlapping-routes',
                 permanent: true,
             },
             {
