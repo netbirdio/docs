765a692034e43dfdd6772bfda94b9d6aa7e1ff99
bcmmbaga <bethuelmbaga12@gmail.com>
2026-06-23 13:56:29 +0300

    wip: add ha docs

--- src/components/NavigationDocs.jsx
+++ src/components/NavigationDocs.jsx
@@ -574,6 +574,10 @@ export const docsNavigation = [
                 title: 'Multiple Proxy Instances',
                 href: '/selfhosted/maintenance/scaling/multiple-proxy-instances',
               },
+              {
+                title: 'High Availability',
+                href: '/selfhosted/maintenance/scaling/high-availability',
+              },
             ],
           },
           { title: 'Backup', href: '/selfhosted/maintenance/backup' },
